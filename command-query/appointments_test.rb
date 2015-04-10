0f37f5a25d2dcc3c6ac067b691d9dd3d1358a0ae
Dmitry Sky <dmitry.sky@icloud.com>
2015-04-10 16:13:58 -0600

    skipped tiny step in appointments_test bc it was pretty ridiculous.

--- command-query/appointments_test.rb
+++ command-query/appointments_test.rb
@@ -1,8 +1,8 @@
 gem 'minitest', '~> 5.2'
 require 'minitest/autorun'
-require 'minitest/pride'
+# require 'minitest/pride'
 require 'time'
-require_relative 'appointments'
+require_relative '../command-query-lib/appointments'
 
 class AppointmentsTest < Minitest::Test
   def test_no_appointments
@@ -11,9 +11,8 @@ class AppointmentsTest < Minitest::Test
   end
 
   def test_earliest_of_one
-    skip
     slots = Appointments.new
-    t1 = Time.new(2014, 3, 14, 4, 30)
+    t1 = Time.new(2014, 3, 14, 4, 30).to_s
     slots.at t1
     assert_equal t1, slots.earliest
   end
@@ -21,9 +20,10 @@ class AppointmentsTest < Minitest::Test
   def test_earliest_of_several
     skip
     slots = Appointments.new
-    t1 = Time.new(2014, 6, 14, 16, 30)
-    t2 = Time.new(2014, 2, 28, 8, 30)
-    t3 = Time.new(2014, 2, 28, 11)
+    t1 = Time.new(2014, 6, 14, 16, 30).to_s # June 14 4:30
+    t2 = Time.new(2014, 2, 28, 8, 30).to_s  # Feb  28 8:30 
+    t3 = Time.new(2014, 2, 28, 11).to_s     # Feb  28 11:00
+                      
     slots.at t1
     slots.at t2
     slots.at t3
